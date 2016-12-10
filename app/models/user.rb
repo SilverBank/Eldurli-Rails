29ed41ad5e8daeedecca8111e9faaa69e35890fe
Connie Cheng <connie.cheng52@gmail.com>
2016-12-10 13:54:51 -0500

    input forms

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,4 @@
 class User < ApplicationRecord
-validates :name, presence: true
-validates :accel_m, presence: true
 
 has_many :debts
 end
--- db/schema.rb
+++ db/schema.rb
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 20161210175036) do
     t.datetime "updated_at", null: false
   end
 
+  create_table "tests", force: :cascade do |t|
+    t.string   "name"
+    t.text     "body"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
   create_table "users", force: :cascade do |t|
     t.string   "name"
     t.integer  "age"
